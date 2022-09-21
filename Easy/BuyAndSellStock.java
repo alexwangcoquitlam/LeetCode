c9013c2b2e3bb9cd52814205e2e8c5f2e458bba2
hwa133 <hwa133@asb9838nu-e11.csil.sfu.ca>
2022-09-21 12:30:32 -0700

    finished #121 best time to buy and sell stock

--- Easy/BuyAndSellStock.java
+++ Easy/BuyAndSellStock.java
@@ -2,37 +2,19 @@ package Easy;
 
 public class BuyAndSellStock {
     public static void main(String[] args){
-        // #121
+        // #121 Best time to buy and sell stock
         int[] input = {7,1,5,3,6,4};
         System.out.println(maxProfit(input));
     }    
 
     public static int maxProfit(int[] prices){
-        // O(n)
-        int buyDay = 0;
+        int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
         for(int i = 0; i < prices.length; i++){
-            if((prices[i] - prices[buyDay]) > maxProfit){
-                maxProfit = prices[i] - prices[buyDay];
-            }
-            else{
-                if(prices[i] < prices[buyDay]){
-                    buyDay = i;
-                }
-            }
+            minPrice = Math.min(prices[i], minPrice);
+            maxProfit = Math.max(prices[i] - minPrice, maxProfit);
         }
         return maxProfit;
-    }
-
-    public static int maxProfitTrivial(int[] prices){
-        // O(n^2)
-        int biggestProfit = 0;
-        for(int i = 0; i < prices.length; i++){
-            for(int j = i; j < prices.length; j++){
-                biggestProfit = Math.max(prices[j] - prices[i], biggestProfit);
-            }
-        }
-        return biggestProfit;
     }
 }
 
