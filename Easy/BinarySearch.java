53b084da56262b7b73a5b75de0d7d7277b802342
hwa133 <hwa133@asb9838nu-e11.csil.sfu.ca>
2022-09-21 13:39:36 -0700

    finished #704 binary search in iterative method

--- Easy/BinarySearch.java
+++ Easy/BinarySearch.java
@@ -3,28 +3,28 @@ package Easy;
 public class BinarySearch {
     // #704
     public static void main(String[] args) {
-        int[] nums = {1};
+        int[] nums = {-1,0,3,5,9,12};
         System.out.println(search(nums, 0));
     }
 
-    public static int search(int[] nums, int target) {
-        return binarySearch(nums, target, nums.length - 1, 0);
-    }
-
-    public static int binarySearch(int[] nums, int target, int high, int low){
-        if(high >= low){
-            int pivot = (int)Math.ceil((high-low)/2)+low;
+    public static int search(int[] nums, int target){
+        int left = 0, right = nums.length-1;
+        int pivot = right/2;
+        while(left <= right){
             if(nums[pivot] == target){
                 return pivot;
             }
-            else if(nums[pivot] > target){
-                return binarySearch(nums, target, pivot-1, low);
-            }
-            else if(nums[pivot] < target){
-                return binarySearch(nums, target, high, pivot+1);
+            else{
+                if(nums[pivot] > target){
+                    right = pivot-1;
+                    pivot = left+(right-left)/2;
+                }
+                else {
+                    left = pivot+1;
+                    pivot = left+(right-left)/2;
+                }
             }
         }
-        
         return -1;
     }
 }
